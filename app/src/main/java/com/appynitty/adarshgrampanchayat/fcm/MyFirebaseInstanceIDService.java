3a8ff36c0a5c43cdf098566577ba0b2588704269
ayan <ayand@appynitty.com>
2019-06-24 18:07:54 +0530

    Testing

--- app/src/main/java/com/appynitty/adarshgrampanchayat/fcm/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/appynitty/adarshgrampanchayat/fcm/MyFirebaseInstanceIDService.java
@@ -22,7 +22,6 @@ import com.appynitty.ghantagaditracker.utils.AUtils;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-import quickutils.core.QuickUtils;
 
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
@@ -43,9 +42,9 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
 
-//        AUtils.saveFcmId(refreshedToken);
+        AUtils.saveFcmId(refreshedToken);
 
-        QuickUtils.prefs.save(AUtils.FCM_ID, FirebaseInstanceId.getInstance().getToken());
+//        QuickUtils.prefs.save(AUtils.FCM_ID, FirebaseInstanceId.getInstance().getToken());
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
